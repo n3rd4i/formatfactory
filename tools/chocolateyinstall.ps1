06cf31e14fb0fafe31de463212b4a4f12d300f74
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-02-27 11:11:48 +0100

    ported from existing version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'formatfactory'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.pcfreetime.com/public/FFSetup4.3.0.0.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'formatfactory*'
+  checksum      = 'C135ED6D5A787840884E0D2E63A9DC8512D854AF0D3442DBE1EAF0EB015044D6'
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
