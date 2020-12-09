9405a636cd66c8371446ec2778421a34be6e8cd3
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-12-09 09:12:44 +0100

    updated to version 5.5.0.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,9 +7,9 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'http://public.pcfreetime.com/FFSetup5.5.0.0.exe'
+  url           = 'http://public.pcfreetime.com/FFSetup5.5.0.1.exe'
   softwareName  = 'formatfactory*'
-  checksum      = 'cce148736fe0f595ee03d86cd7e372d8f1b61c4cfb74c9dfc10a74559f23110d'
+  checksum      = 'b9ce06dbee8fecce2d815d6325a28266f8984f5241f0cbab9243fe07ef47bff1'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
