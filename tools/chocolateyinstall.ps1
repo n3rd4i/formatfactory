e5b6edf9fd87e94a46bc55665979cd694231fd86
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-02-13 16:20:47 +0100

    fixed/working version of the new package manager

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,30 @@
 ﻿$ErrorActionPreference = 'Stop';
-
-$packageName= 'formatfactory'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+# 1. Download bundle
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = "$(Join-Path $ENV:TEMP 'FFSetup.exe')"
   url           = 'http://public.pcfreetime.com/FFSetup5.6.0.0.exe'
-  softwareName  = 'formatfactory*'
   checksum      = 'e981b8f7f838401d38b0c308a3b62a495d0a533b057491138064497a82dfac3a'
   checksumType  = 'sha256'
-  silentArgs   = '/S'
 }
+$filePath = Get-ChocolateyWebFile @packageArgs
 
-Install-ChocolateyPackage @packageArgs
+# 2. Extract bundle
+$packageArgs = @{
+  fileFullPath  = "$filePath"
+  destination   = "$(Join-Path $ENV:TEMP 'FFSetup')"
+}
+$folderPath = Get-ChocolateyUnzip @packageArgs
+
+# 3. Install silent
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = "$(Join-Path $folderPath "Carrier.exe")"
+  softwareName  = 'formatfactory*'
+  silentArgs   = '/S'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs
